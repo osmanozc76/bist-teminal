436c5547cd158aad8c471efe82a3c2595fe3f176
osmanozc76 <osman.ozc76@gmail.com>
2026-05-19 17:03:33 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="BIST Terminal")
+
+st.title("📈 BIST Terminal")
+git add .
+st.write("Sistem aktif 🚀")
